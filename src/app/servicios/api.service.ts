8b1e57971a6415b8d04cdfcaa7a1e33d301cf31a
DanieLazo17 <daniel.lazo92@gmail.com>
2021-12-19 15:19:33 -0300

    modificamos método en http

--- src/app/servicios/api.service.ts
+++ src/app/servicios/api.service.ts
@@ -23,7 +23,7 @@ export class ApiService {
   }
 
   cambiarNombre(id:number, datos:FormData){
-    return this.clienteHttp.patch(this.api + 'Cliente/Nombre/' + id, datos);
+    return this.clienteHttp.put(this.api + 'Cliente/Nombre/' + id, datos);
   }
 
   cambiarApellido(id:number, datos:FormData){
